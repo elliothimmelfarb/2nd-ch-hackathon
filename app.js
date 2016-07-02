6b5cdc27eca3f13bdf04e2a902944436e20485b2
mollywhitnack <mollywhitnack@gmail.com>
2016-07-02 13:07:31 -0700

    branch

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use('/api', require('./routes/api'));
 ////////////////////////////////
 
 app.get('/', (req, res) => {
-  res.render('index', {title: 'Laundry Schedule'});
+  res.render('index');
 });
 
 // catch 404 and forward to error handler
