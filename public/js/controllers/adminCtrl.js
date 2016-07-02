3683d3b3f1cb62479463829162e840360c0167c9
mollywhitnack <mollywhitnack@gmail.com>
2016-07-02 13:03:50 -0700

    tesy

--- public/js/controllers/adminCtrl.js
+++ public/js/controllers/adminCtrl.js
@@ -19,7 +19,6 @@ angular.module('myApp')
        $scope.showUpdate =true;
       console.log("create Schedule")
       console.log("input: ", emails);
-      //split on spaces and commas
       var emalList = emails.split(',');
 
       //remove whitespaces
